fd6e90e07023436a02251bc7f72b093affe4d0f3
karol saenz <karolsaenz82@gmail.com>
2026-06-05 21:22:42 -0500

    4

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-from database import conectar_postgres, coleccion_estudiantes
 
 app = Flask(__name__)
 
@@ -18,52 +17,12 @@ def index():
             programa = request.form["programa"]
             puntaje = float(request.form["puntaje"])
 
-            # Validaciones
-            if not documento or not nombre:
-                raise Exception("Documento y nombre son obligatorios")
-
-            if "@" not in correo:
-                raise Exception("Correo inválido")
-
-            if puntaje < 0 or puntaje > 100:
-                raise Exception("Puntaje fuera de rango")
-
             aplica_beca = "SI" if puntaje >= 85 else "NO"
 
-            # PostgreSQL
-            conexion = conectar_postgres()
-            cursor = conexion.cursor()
-
-            cursor.execute("""
-                INSERT INTO estudiantes
-                (documento,nombre,correo,programa,puntaje,aplica_beca)
-                VALUES (%s,%s,%s,%s,%s,%s)
-            """,
-            (
-                documento,
-                nombre,
-                correo,
-                programa,
-                puntaje,
-                aplica_beca
-            ))
-
-            conexion.commit()
-
-            cursor.close()
-            conexion.close()
-
-            # MongoDB
-            coleccion_estudiantes.insert_one({
-                "documento": documento,
-                "nombre": nombre,
-                "correo": correo,
-                "programa": programa,
-                "puntaje": puntaje,
-                "aplica_beca": aplica_beca
-            })
-
-            mensaje = f"Estudiante registrado. Beca: {aplica_beca}"
+            mensaje = (
+                f"Estudiante {nombre} registrado correctamente. "
+                f"Aplica a beca: {aplica_beca}"
+            )
 
         except Exception as e:
 
